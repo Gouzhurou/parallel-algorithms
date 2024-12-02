aa5b3a7a84dd87c20f3404890d0805e0fbd0ccfd
gouzhurou <arlantamom1999@gmail.com>
2024-12-02 16:44:17 +0300

    2test

--- lab2/rude.cpp
+++ lab2/rude.cpp
@@ -8,160 +8,166 @@
 #include <vector>
 #include <thread>
 
-
-using namespace std;
-
+#define SIZE 30
+#define MATRICES_COUNT 2
+#define RESULT_FILE_NAME "rude_result.txt"
+#define MAX_QUEUE_SIZE 100
 
 template<typename T>
-class QueueRude {
+class QueueRude 
+{
 private:
-    mutable mutex mut;
-    queue<T> my_queue;
-    condition_variable cv;
+    mutable std::mutex mut;
+    std::queue<T> my_queue;
+    std::condition_variable cv;
+    int size;
 public:
-    QueueRude() {}
+    QueueRude() : size(0) {}
 
-    void push(T new_value) {
-        lock_guard<mutex> lk(mut);
+    void push(T new_value) 
+    {
+        std::lock_guard<std::mutex> lk(mut);
         my_queue.push(std::move(new_value));
         cv.notify_one();
     }
-    void wait_and_pop(T& value) {
-        unique_lock<mutex> lk(mut);
-        cv.wait(lk, [this] {return !my_queue.empty();});
+
+    void wait_and_pop(T& value) 
+    {
+        std::unique_lock<std::mutex> lk(mut);
+        cv.wait(lk, [this] {return !is_empty();});
         value = std::move(my_queue.front());
         my_queue.pop();
     }
 
-    bool empty() {
-        lock_guard<mutex> lk(mut);
+    bool is_empty() 
+    {
+        std::lock_guard<std::mutex> lk(mut);
         return my_queue.empty();
     }
 
-    int size() {return my_queue.size();}
+    // int size() {return my_queue.size();}
 };
 
 
 
-vector<vector<int>> generate_matrix(int rows, int columns){
-    vector<vector<int>> matrix(rows, vector<int>(columns, 0));
+std::vector<std::vector<int>> generate_matrix(int rows, int columns)
+{
+    std::vector<std::vector<int>> matrix(rows, std::vector<int>(columns, 0));
 
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < columns; j++){
+    for (auto i = 0; i < rows; i++)
+        for (auto j = 0; j < columns; j++)
             matrix[i][j] = rand() % 100;
-        }
-    }
 
     return matrix;
 }
 
 
-void generate_matrices(QueueRude<vector<vector<int>>*>& qgenerate, int calc_num, int prod_num){
-    for (int i = 0; i < calc_num; i += prod_num){
-        vector<vector<int>>* matrices = new vector<vector<int>>[2];
+void generate_matrices(QueueRude<std::vector<std::vector<int>>*>& qgenerate, int calc_count, int producer_count)
+{
+    for (auto i = 0; i < calc_count; i += producer_count)
+    {
+        std::vector<std::vector<int>>* matrices = new std::vector<std::vector<int>>[MATRICES_COUNT];
 
-        matrices[0] = generate_matrix(30, 30);
-        matrices[1] = generate_matrix(30, 30);
+        matrices[0] = generate_matrix(SIZE, SIZE);
+        matrices[1] = generate_matrix(SIZE, SIZE);
 
         qgenerate.push(matrices);
     }
-
 }
 
-void multiply_matrices(QueueRude<vector<vector<int>>*>& qgenerate,
-                       QueueRude<vector<vector<int>>>& qmult, int calc_num, int cons_num){
-
-    for (int i = 0; i < calc_num; i += cons_num){
-        vector<vector<int>>* matrices;
+void multiply_matrices(QueueRude<std::vector<std::vector<int>>*>& qgenerate,
+                       QueueRude<std::vector<std::vector<int>>>& qmult, int calc_count, int consumer_count)
+{
+    for (auto i = 0; i < calc_count; i += consumer_count)
+    {
+        std::vector<std::vector<int>>* matrices;
         qgenerate.wait_and_pop(matrices);
 
-        int row1 = matrices[0].size();
-        int col1 = matrices[0][0].size();
-        int row2 = matrices[1].size();
-        int col2 = matrices[1][0].size();
+        std::vector<std::vector<int>> matrix_res(SIZE, std::vector<int>(SIZE, 0));
 
-        vector<vector<int>> matrix_res(matrices[0].size(), vector<int>(matrices[1][0].size(), 0));
-
-        for(int i = 0; i < row1; i++){
-            for(int j = 0; j < col2; j++){
-                for(int k = 0; k < col1; k++)
+        for(auto i = 0; i < SIZE; i++)
+            for(auto j = 0; j < SIZE; j++)
+                for(auto k = 0; k < SIZE; k++)
                     matrix_res[i][j] += matrices[0][i][k] * matrices[1][k][j];
-            }
-        }
 
         qmult.push(matrix_res);
         delete[] matrices;
     }
 }
 
-void write_result(QueueRude<vector<vector<int>>>& qmult, int calc_num, string filename){
-
-    for (int i = 0; i < calc_num; i++){
-        vector<vector<int>> matrix_res;
+void write_result(QueueRude<std::vector<std::vector<int>>>& qmult, int calc_count, const std::string& file_name)
+{
+    for (auto i = 0; i < calc_count; ++i)
+    {
+        std::vector<std::vector<int>> matrix_res;
         qmult.wait_and_pop(matrix_res);
 
-        ofstream file;
-        file.open(filename, ios::app);
+        std::ofstream fout;
+        // All output operations happen at the end of the file, appending to its existing contents
+        fout.open(file_name, std::ios::app);
 
-        if (!file){
-            cout << "Can't open file to write result\n";
+        if (!fout){
+            std::cerr << "Can't open fout to write result" << std::endl;
             return;
         }
 
-        file << "--------------------------------" << endl;
+        fout << "--------------------------------" << std::endl;
 
-        for (int i = 0; i < matrix_res.size(); i++){
-            for(int j = 0; j < matrix_res[i].size(); j++){
-                file << matrix_res[i][j] << ' ';
+        for (auto i = 0; i < SIZE; ++i)
+        {
+            for(auto j = 0; j < SIZE - 1; ++j)
+            {
+                fout << matrix_res[i][j] << ' ';
             }
-            file << endl;
+            fout << matrix_res[i][SIZE - 1] << std::endl;
         }
 
-        file.close();
+        fout.close();
     }
 }
 
 
 int main()
 {
-    int calc_num = 300;
-    int repeat_num = 100;
-    int prod_num = 100;
-    int cons_num = 100;
-    unsigned long sum_duration = 0;
+    int calc_count = 10;
+    int repeat_count = 2;
+    int producer_count = 2;
+    int consumer_count = 2;
+    unsigned long duration_sum = 0;
 
-    QueueRude<vector<vector<int>>*>* qgenerate = new QueueRude<vector<vector<int>>*>;
-    QueueRude<vector<vector<int>>>* qmult = new QueueRude<vector<vector<int>>>;
+    QueueRude<std::vector<std::vector<int>>*>* qgenerate = new QueueRude<std::vector<std::vector<int>>*>;
+    QueueRude<std::vector<std::vector<int>>>* qmult = new QueueRude<std::vector<std::vector<int>>>;
 
-    string out_file_name = "/home/master/work/etu/PA/lab2/result.txt";
+    for (auto i = 0; i < repeat_count; ++i)
+    {
+        remove(RESULT_FILE_NAME);
 
-    for (int i = 0; i < repeat_num; i++){
-        remove(out_file_name.c_str());
+        auto start_time = std::chrono::high_resolution_clock::now();
 
-        auto start_time = chrono::high_resolution_clock::now();
+        std::vector<std::thread> threads;
 
-        vector<thread> all_threads;
+        for (int j = 0; j < producer_count; j++)
+            threads.push_back(std::thread(generate_matrices, std::ref(*qgenerate), calc_count, producer_count));
 
-        for (int j = 0; j < prod_num; j++)
-            all_threads.push_back(thread(generate_matrices, ref(*qgenerate), calc_num, prod_num));
+        for (int j = 0; j < consumer_count; j++)
+            threads.push_back(std::thread(multiply_matrices, std::ref(*qgenerate), std::ref(*qmult), calc_count, consumer_count));
 
-        for (int j = 0; j < cons_num; j++)
-            all_threads.push_back(thread(multiply_matrices, ref(*qgenerate), ref(*qmult), calc_num, cons_num));
+        threads.push_back(std::thread(write_result, std::ref(*qmult), calc_count, RESULT_FILE_NAME));
 
-        all_threads.push_back(thread(write_result, ref(*qmult), calc_num, out_file_name));
-
-        for (int j = 0; j < prod_num + cons_num + 1; j++) {
-            all_threads[j].join();
+        for (auto j = 0; j < producer_count + consumer_count + 1; ++j) 
+        {
+            threads[j].join();
         }
 
-        auto stop_time = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+        auto stop_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time);
 
-        sum_duration += duration.count();
+        duration_sum += duration.count();
     }
 
-    cout << "Executing time:" << sum_duration / repeat_num << endl;
+    std::cout << "Executing time:" << duration_sum / repeat_count << std::endl;
 
     delete qgenerate;
     delete qmult;
 }
+
